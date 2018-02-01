f96ee7e889e83c466767ae9f3e44ebe30a17e776
ZSaberLv0 <z@zsaber.com>
2018-02-01 11:26:55 +0800

    first commit

--- vim-plug-automerge.sh
+++ vim-plug-automerge.sh
@@ -1,11 +1,12 @@
 WORK_DIR=$(cd "$(dirname "$0")"; pwd)
 
-if test "x-$ZFDOC_KEY" = "x-" ; then
-    echo "ZFDOC_KEY not set"
+if test "x-$MYCI_KEY" = "x-" ; then
+    echo "MYCI_KEY not set"
     exit 1
 fi
 
 _OLD_DIR=$(pwd)
+rm -rf vim-plug
 git clone https://github.com/ZSaberLv0/vim-plug
 cd vim-plug
 git config user.email "z@zsaber.com"
@@ -15,6 +16,6 @@ git config push.default "simple"
 git remote add upstream https://github.com/junegunn/vim-plug
 git fetch upstream
 git merge upstream/master --no-edit
-git push "https://ZSaberLv0:$ZFDOC_KEY@github.com/ZSaberLv0/vim-plug"
+git push "https://ZSaberLv0:$MYCI_KEY@github.com/ZSaberLv0/vim-plug"
 cd "$_OLD_DIR"
 
